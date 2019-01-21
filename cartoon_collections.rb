e5151f4be9954bb949e33db791871ccc7e05a60c
Herman <hrivas5726@aol.com>
2019-01-21 05:06:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,15 @@ def summon_captain_planet(array) #code an argument here
   new_array
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array) # code an argument here
   # Your code here
+   i = 0 
+  if  array.any? {|i| i.length > 4}
+    return true
+  else 
+    return false
+  i = i + 1
+  end 
 end
 
 def find_the_cheese# code an argument here
