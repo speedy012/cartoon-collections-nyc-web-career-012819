1e4778b65521e139cca0e55a1b0fcc7c9b6cc574
Herman <hrivas5726@aol.com>
2019-01-21 05:08:02 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -32,4 +32,7 @@ end
 def find_the_cheese(array # code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+   array.find do |type|
+    cheese_types.include?(type)
+  end
 end
